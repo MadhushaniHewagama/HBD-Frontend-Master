5738a8c124b3b233177cfad9e47caea5db72fb67
MadhushaniHewagama <hewagamamadhushani@gmail.com>
2019-11-10 20:22:49 +0530

    bug fixed

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -29,7 +29,7 @@ export class SignUpPage implements OnInit {
 
       email: new FormControl("", [Validators.required, Validators.email]),
 
-      password : new FormControl([Validators.required
+      password : new FormControl("",[Validators.required
       ])
     })
 
