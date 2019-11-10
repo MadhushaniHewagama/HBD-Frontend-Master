1efcde66fe859b79aa38dab6f8baddae03912590
RPIUdishan <it18082234@my.sliit.lk>
2019-11-10 19:04:55 +0530

    register method implemented

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HdbService } from 'src/app/services/hdb.service';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  
+  public signUpForm : FormGroup;
 
-  constructor() { }
+  constructor(private hbdService:HdbService) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  public createForm():void{
+
+    this.signUpForm = new FormGroup({
+      user_name: new FormControl("",[
+        Validators.required,
+       
+        
+      ]),
+
+      email: new FormControl("", [Validators.required, Validators.email]),
+
+      password : new FormControl([Validators.required
+      ])
+    })
+
+  }
+
+  register():void{
+    if(true){
+      console.log(JSON.stringify(this.signUpForm.value));
+      this.hbdService.addUser(this.signUpForm.value).subscribe(
+        res => { console.log(res); },
+        err => { console.log(err); }
+
+      )
+    }
   }
 
 }
