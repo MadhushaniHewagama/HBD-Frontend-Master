c54a81558a701ce79b32210504edbc51a10415cd
Madhushani Hewagama <madhushani.hewagama@ge.com>
2019-11-27 16:34:43 +0530

    sample events

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -36,39 +36,24 @@ export class HomePage {
     this.eventSource = this.createRandomEvents();
   }
   createRandomEvents() {
-    var events = [];
-    for (var i = 0; i < 50; i += 1) {
-        var date = new Date();
-        var eventType = Math.floor(Math.random() * 2);
-        var startDay = Math.floor(Math.random() * 90) - 45;
-        var endDay = Math.floor(Math.random() * 2) + startDay;
-        var startTime;
-        var endTime;
-        if (eventType === 0) {
-            startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
-            if (endDay === startDay) {
-                endDay += 1;
-            }
-            endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
-            events.push({
-                title: 'Test first type event' + i,
-                startTime: startTime,
-                endTime: endTime,
-                allDay: true
-            });
-        } else {
-            var startMinute = Math.floor(Math.random() * 24 * 60);
-            var endMinute = Math.floor(Math.random() * 180) + startMinute;
-            startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
-            endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
-            events.push({
-                title: 'Test second type event' + i,
-                startTime: startTime,
-                endTime: endTime,
-                allDay: false
-            });
-        }
-    }
+    var date = new Date();
+    var events = [{
+      title: 'Meeting 1',
+      startTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1)),
+      endTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 2)),
+      allDay: true
+  },{
+    title: 'Meeting 2',
+    startTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 3)),
+    endTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 4)),
+    allDay: true
+},{
+  title: 'Meeting 3',
+  startTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() - 2)),
+  endTime: new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() - 1)),
+  allDay: true
+}];
+    
     return events;
 }
   onCurrentDateChanged(ev) {
